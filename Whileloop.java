44dc24e32369c4b72e26fcd8b4deeee578f8852f
Tmoise221 <149689559+Tmoise222014761@users.noreply.github.com>
2023-11-03 13:27:10 +0200

    Add files via upload
    jAVA WHILE LOOP

---
+++ Whileloop.java
@@ -0,0 +1,13 @@
+//Java programs that prints that counts from 1 to 12.
+public class Whileloop { //opens a class named While loop.
+    public static void main(String[] args) { //opens a method.
+        int a=1; //initialisation of variable a to 1.
+        while(a<=12){  //creates and opens a while loop.
+            System.out.println(a); // prints values of a.
+            a++;  //increments variable a.    
+
+     } //closes a while loop.
+
+    } //closes a method.
+    
+}//closes a class.
