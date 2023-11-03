91f7bc3786cb906b11348d361f875c50651d726a
Tmoise221 <149689559+Tmoise222014761@users.noreply.github.com>
2023-11-03 13:23:33 +0200

    Add files via upload
    JAVA FORLOOP PROJECT

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+//Java program that uses for loop to print multiples of 2
+public class Forloop { //creates and opens a class named Forloop.
+    public static void main(String[] args) { //opens a method.
+        System.out.println("Multiples of 2 till 20");//displays a message:"Multiples of 2 till 20".
+        int i=1; //initialises variable i to 1.
+        for(i=1;i<=10;i++) //declares a for loop
+        { //opens our for loop.
+            System.out.println(2*i); // prints values of 2*i.
+        } // closes a for loop
+     
+    } //closes a method.
+    
+} // closes a class.
