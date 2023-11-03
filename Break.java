58efd44745a34ede0b0974c324da559da2610bea
Tmoise221 <149689559+Tmoise222014761@users.noreply.github.com>
2023-11-03 13:29:47 +0200

    Add files via upload
    Java Break project

---
+++ Break.java
@@ -0,0 +1,15 @@
+//java project that uses break.
+public class Break { // creates and opens a class named Break.
+    public static void main(String[] args) {  //Mandatory statement in java programs. Opens a method.
+        for(int i=1;i<=10;i++)
+        { //opens for loop.
+            if(i==5){
+                break; //breaks the for loop when i is equal to 5.
+            }
+            System.out.println(i);//prints the values of i.
+        }//closes for loop.
+    }//closes method.
+
+    
+} //closes a class.
+ 
