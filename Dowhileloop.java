5152c7330b197a3e78bd87df0467ac5c5dd74381
Tmoise221 <149689559+Tmoise222014761@users.noreply.github.com>
2023-11-03 16:00:14 +0200

    Add files via upload
    Dowhileloop java project

---
+++ Dowhileloop.java
@@ -0,0 +1,13 @@
+//Java project that uses do while to print numbers from to 10
+public class Dowhileloop { //declares and the main class named Dowhileloop
+    public static void main(String[] args) { // opens a main method.
+        int i=1; // initialisation of variable i with value 1 aand datatype integer.
+        do{
+            System.out.println(i); //prints the values of i.
+            i++; //incrementarion of values of i.
+        }
+        while(i<=10); // while loop to test condition after executing statements.
+
+    } //closes the main method.
+    
+} // closes the main class.
