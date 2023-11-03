9256db93468b2ffed7021198909f603a0e962cce
Tmoise221 <149689559+Tmoise222014761@users.noreply.github.com>
2023-11-03 12:16:01 +0200

    Add files via upload
    Java project that uses if condition

---
+++ Cwork1.java
@@ -0,0 +1,10 @@
+//java program to decide the greater value among the two values.
+public class Cwork1 { //declares and opens class named Cwork1.
+    public static void main(String[] args) { //declares a method.
+        int a=10;  //initialises variable a with value 10.
+        int b=15;  //initialises variable b with value 15;
+        if(b>a){ //creates and opens a condition.
+            System.out.println("b is greater");//prints output
+        } //closes if
+    } //closes a method.    
+} //closes a class.
