a8eeb0a8cf483e76e65efddcf7a5250ed38c55ed
Tmoise221 <149689559+Tmoise222014761@users.noreply.github.com>
2023-11-03 12:18:56 +0200

    Add files via upload
    Switch case statement java project

---
+++ Cwork3.java
@@ -0,0 +1,26 @@
+class Cwork3 {
+    // java program that uses switch case statement 
+    public static void main(String[] args)
+    { //opens a mandatory method.
+    int age=13; //intialisation of variable a with value 13;
+    switch(age) //declares a switch.
+    { //opens a switch case statements.
+        case 13: //opens a case 13;
+        System.out.println("You are Young");//output for case 13.
+        break; //closes case 13;
+         case 20: //closes case 20;
+         System.out.println("You are Mature");//output for case 20.
+         break; //closes case 20;
+         case 30://closes case 30;
+          System.out.println("You are Adult"); //output for case 30.
+          break;//closes case 30;
+          case 40://closes case 40;
+           System.out.println("You are Old"); //output for case 40.
+           break; //closes case 40;
+           default:  //opens a default case.
+            System.out.println("Your age is not in"); //default output.
+            break;  //closes a default case.
+
+    } //closes a switch statements.
+}
+} //closes a class
